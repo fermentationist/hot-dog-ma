ee7ac3cc57578877a2bed984b5c2b304c966ef8e
Dennis Hodges <fermentationist@gmail.com>
2022-12-16 15:47:08 -0600

    Removed accidentally committed .env file

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 import "dotenv/config";
 import express from "express";
+import axios from "axios";
 import rateLimiter from "./rateLimiter.js";
 import { fileURLToPath } from "url";
 import path, { dirname } from "path";
 import apiRouter from "./api/router.js";
 import { API_SERVER_PORT } from "../vite.config.js";
+
+const SELF_URL = "https://hot-dog-ma.onrender.com";
+const WAKE_SERVER_INTERVAL = 1000 * 60 * 14; // 14 minutes
 
 const PROD_MODE = process.env.PROD_MODE === "true";
 const STATIC_FOLDER = PROD_MODE ? "../build/" : "../";
@@ -23,6 +27,16 @@ app.use("/api", rateLimiter, apiRouter);
 
 app.listen(API_SERVER_PORT, () => {
   console.log(`Express app listening on port ${API_SERVER_PORT}`);
+
+  setInterval(() => {
+    // keep server awake
+    console.log(`Fetching ${SELF_URL} to wake up server`)
+    axios(SELF_URL)
+      .then(() =>
+        console.log("Server is awake")
+      )
+      .catch((error) => console.log(`Error fetching ${SELF_URL}: ${error.message}`));
+  }, WAKE_SERVER_INTERVAL);
 });
 
 
