a4226a232387830277201ba0be4d714c0850fc78
Dennis Hodges <fermentationist@gmail.com>
2022-12-20 11:48:51 -0600

    Hard-coded url in sharePrayer function

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -14,7 +14,7 @@ import { parseQueryString } from "../../utils/helpers.js";
 
 const INTRO = `Enter a subject (a word or short phrase) and click "submit" to generate a unique "prayer" using the OpenAI GPT-3 Chatbot.`;
 
-const DISCLAIMER = `Intended for entertainment purposes only. Limit of 15 requests per hour. Prayers are NOT guaranteed to work. (Not unless you REALLY mean it.) No claims are made regarding the authenticity of generated prayers in relation to the selected target religion. Generated prayers may contain elements of hot dog orthodoxy. Use at your own spiritual risk.`
+const DISCLAIMER = `Intended for entertainment purposes only. Limit of 15 requests per hour. Prayers are NOT guaranteed to work. (Not unless you REALLY mean it.) No claims are made regarding the authenticity of generated prayers in relation to the selected target religion. Generated prayers may contain elements of hot dog orthodoxy. Use at your own spiritual risk.`;
 
 const StyledPage = styled(Page)`
   display: flex;
@@ -59,24 +59,32 @@ const ShareIconContainer = styled.div`
   cursor: pointer;
 `;
 
-const Home = ({
-  startLoading,
-  doneLoading
-}) => {
+const Home = ({ startLoading, doneLoading }) => {
   const [prayer, setPrayer] = useState("");
   const selectedReligion = useRef("Christianity");
   const inputRef = useRef(null);
   const hotDogRef = useRef(true);
-  const {alertError, callAlertProm, callAlert} = useAlert();
-  const {data: prayerData, error, loading, refresh, updateUrl} = useFetch(null);
+  const { alertError, callAlertProm, callAlert } = useAlert();
+  const {
+    data: prayerData,
+    error,
+    loading,
+    refresh,
+    updateUrl,
+  } = useFetch(null);
 
   useEffect(() => {
-    const [prompt, style, hotdog] = parseQueryString(["prompt", "style", "hotdog"], window.location.search);
+    const [prompt, style, hotdog] = parseQueryString(
+      ["prompt", "style", "hotdog"],
+      window.location.search
+    );
     if (prompt) {
       inputRef.current.value = decodeURIComponent(prompt.replace(/\+/g, " "));
       selectedReligion.current = decodeURIComponent(style);
       hotDogRef.current = hotdog === "false" ? false : true;
-      updateUrl(`/api/openai/prayer?prompt=${prompt}&style=${style}&hotdog=${hotdog}`);
+      updateUrl(
+        `/api/openai/prayer?prompt=${prompt}&style=${style}&hotdog=${hotdog}`
+      );
     } else {
       callAlertProm(INTRO).then(() => inputRef.current.focus());
     }
@@ -99,7 +107,7 @@ const Home = ({
     }
     return () => {
       clearTimeout(focusTimeout);
-    }
+    };
   }, [startLoading, doneLoading, prayerData, loading, error]);
 
   const submit = () => {
@@ -110,50 +118,60 @@ const Home = ({
       searchParams.set("style", selectedReligion.current);
       searchParams.set("hotdog", hotDogRef.current);
       const [url] = window.location.href.split("?");
-      window.history.replaceState({}, null, `${url}?${searchParams.toString()}`);
+      window.history.replaceState(
+        {},
+        null,
+        `${url}?${searchParams.toString()}`
+      );
       updateUrl(`/api/openai/prayer?${searchParams.toString()}`);
     }
   };
 
-  const religionSelectorCallback = religion => {
+  const religionSelectorCallback = (religion) => {
     selectedReligion.current = religion;
     submit();
-  }
+  };
 
-  const onKeyDown = event => {
+  const onKeyDown = (event) => {
     const prompt = inputRef.current.value;
     if (event.key === "Enter" && prompt) {
       event.preventDefault();
       submit();
     }
-  }
+  };
 
-  const hotDogSelectorCallback = boolean => {
+  const hotDogSelectorCallback = (boolean) => {
     hotDogRef.current = boolean;
     submit();
-  }
+  };
 
   const copyPrayer = () => {
     navigator.clipboard.writeText(prayer);
     callAlert("Prayer copied to clipboard");
-  }
+  };
 
   const sharePrayer = async () => {
     let queryString = window.location.search;
     if (!queryString) {
-      queryString = `?prompt=${encodeURIComponent(inputRef.current.value)}&style=${encodeURIComponent(selectedReligion.current)}&hotdog=${encodeURIComponent(hotDogRef.current)}`
+      queryString = `?prompt=${encodeURIComponent(
+        inputRef.current.value
+      )}&style=${encodeURIComponent(
+        selectedReligion.current
+      )}&hotdog=${encodeURIComponent(hotDogRef.current)}`;
     }
-    const [url] = window.location.href.split("?");
+    // const [url] = window.location.href.split("?");
+    const url = "https://www.hotdogisasandwich.com"
     const shareData = {
       url: encodeURI(url + queryString),
       title: "Hot Dog-ma Prayer Generator",
       text: `🙏🏻🙏🏼🙏🏽🙏🏾🙏🏿🌭🌭🌭${inputRef.current.value}🌭🌭🌭🙏🏿🙏🏾🙏🏽🙏🏼🙏🏻`,
-    }
-    const canShare = "canShare" in navigator && await navigator.canShare(shareData);
+    };
+    const canShare =
+      "canShare" in navigator && (await navigator.canShare(shareData));
     if (canShare) {
       await navigator.share(shareData);
     }
-  }
+  };
 
   return (
     <StyledPage>
@@ -169,52 +187,52 @@ const Home = ({
           />
           <Button onClick={submit}>SUBMIT</Button>
         </InputContainer>
-        <ReligionSelector callback={religionSelectorCallback} defaultSelected={selectedReligion.current}/>
+        <ReligionSelector
+          callback={religionSelectorCallback}
+          defaultSelected={selectedReligion.current}
+        />
         <HotDogSelector
           callback={hotDogSelectorCallback}
           defaultChecked={hotDogRef.current}
         />
-        <br/>
+        <br />
         <PrayerContainer>
-          {
-            prayer.length ? (
-              <>
-                <CopyIconContainer>
-                  <CopyIcon onClick={copyPrayer}/>
-                </CopyIconContainer>
-                {
-                  "canShare" in navigator ? (
-                    <ShareIconContainer>
-                      <ShareIcon onClick={sharePrayer} />
-                    </ShareIconContainer>
-                  ) : null
-                }
-              </>
-            ) : null
-          }
-          <br/>
-          {
-            prayer.split("\n\n").map((stanza, stanzaIndex) => {
-              return (
-                <p key={stanzaIndex}>
-                  {
-                    stanza.split("\n").map((line, lineIndex) => {
-                      return (
-                        <Fragment key={lineIndex}>
-                          <span key={lineIndex}>{line}</span>
-                          <br/>
-                        </Fragment>
-                      )
-                    })
-                  }
-                </p>
-              )
-            })
-          }
+          {prayer.length ? (
+            <>
+              <CopyIconContainer>
+                <CopyIcon onClick={copyPrayer} />
+              </CopyIconContainer>
+              {"canShare" in navigator ? (
+                <ShareIconContainer>
+                  <ShareIcon onClick={sharePrayer} />
+                </ShareIconContainer>
+              ) : null}
+            </>
+          ) : null}
+          <br />
+          {prayer.split("\n\n").map((stanza, stanzaIndex) => {
+            return (
+              <p key={stanzaIndex}>
+                {stanza.split("\n").map((line, lineIndex) => {
+                  return (
+                    <Fragment key={lineIndex}>
+                      <span key={lineIndex}>{line}</span>
+                      <br />
+                    </Fragment>
+                  );
+                })}
+              </p>
+            );
+          })}
         </PrayerContainer>
       </MainContainer>
-      <Footer disclaimerCallback={() => callAlertProm(DISCLAIMER).then(() => inputRef.current.focus())}/>
-    </StyledPage>);
+      <Footer
+        disclaimerCallback={() =>
+          callAlertProm(DISCLAIMER).then(() => inputRef.current.focus())
+        }
+      />
+    </StyledPage>
+  );
 };
 
 export default withLoadingSpinner(Home);
